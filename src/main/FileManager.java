9c2931e5a2bd28f1ad389c2408c99327962bb881
ProfessorKho <alexandre.caux@live.fr>
2023-04-26 10:41:36 +0200

    FileManager opérationnel (manque gestion d'erreurs)

--- src/main/FileManager.java
+++ src/main/FileManager.java
@@ -3,14 +3,13 @@ package main;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-
-
+import java.util.Objects;
 public class FileManager {
     public File pathsource;
     public File pathdestination;
 
-    public void copy(File ps,File pd) throws IOException {
-        for(File f : ps.listFiles()){
+    public static void copy(File ps,File pd){
+        for(File f : Objects.requireNonNull(ps.listFiles())){
             File sc = new File(ps,f.getName());
             File dc = new File(pd,f.getName());
             if(f.isDirectory()){
@@ -25,9 +24,51 @@ public class FileManager {
             else {
                 if(dc.exists()) {
                     //check the last modified date
+                    if(!(sc.lastModified() == dc.lastModified())){
+                        dc.delete();
+                        try {
+                            Files.copy(sc.toPath(), dc.toPath());
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
                 }
                 else {
-                    Files.copy(sc.toPath(), dc.toPath());
+                    try {
+                        Files.copy(sc.toPath(), dc.toPath());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+    }
+
+    public static Boolean deleteDirectory(File directoryToBeDeleted) {
+        File[] allContents = directoryToBeDeleted.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteDirectory(file);
+            }
+        }
+        return directoryToBeDeleted.delete();
+    }
+
+    public static void rmdiff(File ps,File pd){
+        for(File f : Objects.requireNonNull(pd.listFiles())){
+            File sc = new File(ps,f.getName());
+            File dc = new File(pd,f.getName());
+            if(f.isDirectory()){
+                if(sc.exists()){
+                    rmdiff(sc,dc);
+                }
+                else{
+                    deleteDirectory(dc);
+                }
+            }
+            else {
+                if(!(sc.exists())) {
+                    dc.delete();
                 }
             }
         }

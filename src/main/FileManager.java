66e1c9265bacd025571cd15a1c91185cec90e73c
ProfessorKho <alexandre.caux@live.fr>
2023-04-28 13:32:20 +0200

    FileManager avec thread et détection de la source automatique (attention à la gestion d'erreurs)

--- src/main/FileManager.java
+++ src/main/FileManager.java
@@ -4,9 +4,32 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Objects;
-public class FileManager {
-    public File pathsource;
-    public File pathdestination;
+
+public class FileManager extends Thread{
+    public File filea;
+    public File fileb;
+    public FileManager(String a, String b){
+        this.filea = new File(a);
+        this.fileb = new File(b);
+    }
+
+    public void run(){
+        while(true){
+            sychronize(this.filea,this.fileb);
+            try {
+                Thread.sleep(60000);
+            }
+            catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        String a = "C:\\Users\\alexa\\OneDrive\\Travail\\1_Info\\1.12 Java\\Projet\\TestsLocal\\one";
+        String b = "C:\\Users\\alexa\\OneDrive\\Travail\\1_Info\\1.12 Java\\Projet\\TestsLocal\\two";
+        new FileManager(a,b).start();
+    }
 
     public static void copy(File ps,File pd){
         for(File f : Objects.requireNonNull(ps.listFiles())){
@@ -74,11 +97,30 @@ public class FileManager {
         }
     }
 
-    //file.exists()
-    //file.isDirectory()
-    //file.listFiles()
-    //file.mkdir()
-    //Files.copy(src, dst, StandardCopyOption.REPLACE_EXISTING);
-    //abstract pathname : java.io.File
-    //pathname string : java.lang.String
+    public static File whatsource(File a,File b){
+        if(a.lastModified() > b.lastModified()){
+            return a;
+        }
+        else if(a.lastModified() < b.lastModified()){
+            return b;
+        }
+        else{ //Files are already synchronized
+            return null;
+        }
+
+    }
+
+    public void sychronize(File a, File b){
+        if(whatsource(a,b)==null){
+            return;
+        }
+        else if(whatsource(a,b)==a){
+            copy(a,b);
+            rmdiff(a,b);
+        }
+        else{
+            copy(b,a);
+            rmdiff(b,a);
+        }
+    }
 }
